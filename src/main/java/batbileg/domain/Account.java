a07d941e0801b62562781c623a5b4bff0b70be2a
batbileg <batbileg289@gmail.com>
2019-01-24 21:23:38 -0500

    constraint

--- src/main/java/batbileg/domain/Account.java
+++ src/main/java/batbileg/domain/Account.java
@@ -2,6 +2,7 @@ package batbileg.domain;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -16,6 +17,7 @@ public class Account {
 	/**
 	 * Account number
 	 */
+	@Column(nullable = false, unique = true)
 	private String acntNum;
 	
 	/**
